e4126ff5a156ac37d25c49e9d8fc7aaf9fb93924
Katarzyna Biernat <katarzyna.biernat@spot.pl>
2014-03-06 08:06:53 +0100

    Project setup and docs

---
+++ ManagerApp/ManagerApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ManagerApp.Startup))]
+namespace ManagerApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
