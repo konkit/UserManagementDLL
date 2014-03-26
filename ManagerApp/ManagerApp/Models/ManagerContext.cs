a353c3a9ea626547f2e63976c45f1f3a1c9863a3
Katarzyna Biernat <katarzyna.biernat@spot.pl>
2014-03-26 13:39:57 +0100

    DLL and model setup

---
+++ ManagerApp/ManagerApp/Models/ManagerContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using UserDataLib.Models;
+
+namespace ManagerApp.Models
+{
+    public class ManagerContext : DbContext
+    {
+        public ManagerContext()
+            : base("DefaultConnection")
+        {
+
+        }
+
+        public DbSet<User> User { get; set; }
+
+        
+    }
+}
