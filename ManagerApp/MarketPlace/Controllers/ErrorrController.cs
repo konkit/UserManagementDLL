7bd0e90a95115af82217c5e47c31f03bcf9279dc
Paweł Kluba <oskpkluba@gmail.com>
2014-06-30 20:43:56 +0200

    adding marketplace

---
+++ ManagerApp/MarketPlace/Controllers/ErrorrController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MarketPlace.Controllers
+{
+    public class ErrorrController : Controller
+    {
+        //
+        // GET: /Error/
+        public ActionResult AccessDenied()
+        {
+            return View();
+        }
+    }
+}
